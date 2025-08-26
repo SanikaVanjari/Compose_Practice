e5fb6e02bbad6223a83ca11a0233f1ec75740594
S Vanjari <sanikavanjari94@gmail.com>
2025-08-26 21:56:19 +0530

    order of modifier

--- app/src/main/java/com/example/compose1/MainActivity.kt
+++ app/src/main/java/com/example/compose1/MainActivity.kt
@@ -4,16 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.compose1.ui.theme.Compose1Theme
 
 class MainActivity : ComponentActivity() {
@@ -33,9 +38,11 @@ fun Greeting(name: String) {
     Text(
         text = "Hello $name!",
         modifier = Modifier
-            .size(200.dp)
-            .clickable(onClick = {})
+            .border(2.dp, color = Color.Red)
             .padding(30.dp)
+            .border(4.dp, color = Color.Blue)
+            .padding(50.dp),
+        style = MaterialTheme.typography.bodyMedium
     )
 }
 

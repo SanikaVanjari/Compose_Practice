2616774cb6badfa0f2bc6e86854224ce81e27f15
S Vanjari <sanikavanjari94@gmail.com>
2025-08-26 21:42:04 +0530

    order of modifier

--- app/src/main/java/com/example/compose1/MainActivity.kt
+++ app/src/main/java/com/example/compose1/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose1.ui.theme.Compose1Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +21,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Compose1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Greeting(
+                name = "Android"
+            )
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = Modifier
+            .size(200.dp)
+            .clickable(onClick = {})
+            .padding(30.dp)
     )
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Compose1Theme {
-        Greeting("Android")
-    }
+    Greeting("Android")
 }

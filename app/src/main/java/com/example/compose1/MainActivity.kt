63b54b6bc55d265bdb11c5b0e85369276180178f
S Vanjari <sanikavanjari94@gmail.com>
2025-08-26 23:46:26 +0530

    alignment for wrapped content

--- app/src/main/java/com/example/compose1/MainActivity.kt
+++ app/src/main/java/com/example/compose1/MainActivity.kt
@@ -9,10 +9,13 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -26,28 +29,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Greeting(
-                name = "Android"
-            )
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(
-        text = "Hello $name!",
-        modifier = Modifier
-            .border(2.dp, color = Color.Red)
-            .padding(30.dp)
-            .border(4.dp, color = Color.Blue)
-            .padding(50.dp),
-        style = MaterialTheme.typography.bodyMedium
-    )
+fun MainScreen(modifier: Modifier = Modifier) {
+    Surface(color = Color.DarkGray, modifier = Modifier.fillMaxSize()) {
+        Surface(color = Color.Magenta, modifier = Modifier.wrapContentSize(align = Alignment.TopEnd)) {
+            Text(
+                text = "Wrapped Content",
+                style = MaterialTheme.typography.headlineMedium
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Greeting("Android")
+    MainScreen()
 }

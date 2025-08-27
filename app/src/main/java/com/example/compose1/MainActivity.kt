95f619e19b0c411e6c828b4fbd0dacc3eef9589b
S Vanjari <sanikavanjari94@gmail.com>
2025-08-27 20:05:04 +0530

    recomposition

--- app/src/main/java/com/example/compose1/MainActivity.kt
+++ app/src/main/java/com/example/compose1/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.compose1
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,31 +12,55 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.compose1.ui.theme.Compose1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MainScreen()
+            GreetingListView()
         }
     }
+}
+
+@Composable
+fun GreetingListView(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        GreetingList()
+    }
+}
+
+@Composable
+fun GreetingList() {
+    val greetingStateList = remember { mutableStateListOf<String>("Sani") }
+    for (name in greetingStateList) {
+        Greeting(name)
+    }
+    Button(onClick = { greetingStateList.add("New Name") }) {
+        Text(text = "Add new name")
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(text = "Hello $name")
 }
 
 @Composable
@@ -75,5 +98,5 @@ fun HorizontalColorBar(color: Color) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MainScreen()
+    GreetingListView()
 }
